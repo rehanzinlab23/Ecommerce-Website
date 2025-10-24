e4345d46a4efcfe555fc86086439a1d73560f36b
rehanzinlab23 <rehan.zinlab@gmail.com>
2025-10-24 12:15:43 +0500

    done

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const close = document.getElementById('close');
 
 const nav = document.getElementById('navbar');
 
+const remove = document.querySelectorAll('.remove');
+
 if (bar) {
      bar.addEventListener('click', () => {
           nav.classList.add('active')
@@ -17,3 +19,13 @@ if (close) {
           nav.classList.remove('active')
      })
 }
+
+remove.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        // find the closest table row (tr) and remove it
+        const row = btn.closest('tr');
+        if (row) {
+            row.remove();
+        }
+    });
+});
